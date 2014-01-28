e9aa759fb286b3ea1eb04d01646c765bc63afd07
user <user@em5-dev.(none)>
2014-01-28 20:40:44 +0400

    registers in debugfs

---
+++ xlregs.h
@@ -0,0 +1,35 @@
+#define XLBASE	0x0C000000L
+
+#define XL_DATA 	0x0 		// 4-byte data register
+#define XL_PCHI 	0x4 		// write 1 to start pchi 
+#define XL_PCHN 	0x8 		// write 1 to start pchn
+#define XL_STAT 	0x10		// stat register
+#define XL_IFR  	0x14		// IFR interrupt flag register (rw)
+#define XL_CTRL 	0x18		// control_register (rw)
+#define XL_APWR 	0x20		// start AP write
+#define XL_APRD 	0x30		// start AP read
+
+/* Control register bits for EM5:*/
+#define TRIG_ENA  	1<<6	// Enable pchi by front panel input
+#define PROG_BUSY  	1<<5	// Set busy output
+#define FE_ENA  	1<<4
+#define FF_ENA  	1<<3
+#define ES_ENA  	1<<2
+#define BS_ENA  	1<<1	
+#define DMA_ENA  	1<<0	// Enable DREQ2
+
+/* Status register */
+#define FF_EMPTY  	0x1
+#define WRCOUNT_MASK  	0x3FF
+#define WRCOUNT_SHIFT 	16
+/* fifo write count; decreases while reading fifo */
+#define WRCOUNT(stat)  (((stat) >> WRCOUNT_SHIFT ) & WRCOUNT_MASK) 
+
+
+/* Interrupt flag register */
+#define IFR_BS		1<<1	// Begin-of-Spill
+#define IFR_ES		1<<2	// End-of-Spill
+#define IFR_FF		1<<3	// fifo almost full
+#define IFR_FE		1<<4	// fifo empty
+
+
